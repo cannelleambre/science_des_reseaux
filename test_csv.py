2b404f3c9bdab394886e5fe9dfe46e6e0c752acf
Ambre <150542892+cannelleambre@users.noreply.github.com>
2025-04-01 18:02:31 +0200

    Update test_csv.py

--- test_csv.py
+++ test_csv.py
@@ -1,4 +1,5 @@
 import csv
+import math
 
 nb_ligne = 0
 filename = "generated.csv"
@@ -10,3 +11,18 @@ with open(filename) as csvfile:
 
         nb_ligne +=1
 
+def calcul_distance(longitude1, latitude1, longitude2, latitude2):
+    longitude = abs(longitude1 - longitude2)
+    latitude = abs(longitude1 - longitude2)
+    distance = math.sqrt(longitude**2 + latitude**2)
+    return distance
+
+#ALGO
+# 1ère methode 
+# lit ligne par ligne le fichier
+# sur la ligne courante, on regarde si le terminal peut appartenir à cluster voisin
+    # distance < 45 km du centre
+    # debit acceptable par le beam
+# si oui on l'y ajoute
+    # liste ?
+# sinon on le créé
