d968858c489f6fbf85002d65590898feadaaceb5
Ambre Liabat <ammbre.liabat@yahoo.com>
2025-05-14 09:16:26 +0200

    clean ball tree

--- balltree.py
+++ balltree.py
@@ -12,7 +12,7 @@ def run_ball_tree(csv_file, threshold_pir):
         'LAT': np.float32,
         'LON': np.float32
     }
-    donnees = pd.read_csv("generated.csv", delimiter=",", dtype=dtype_dict)
+    donnees = pd.read_csv(csv_file, delimiter=",", dtype=dtype_dict)
 
     # Initialisation des colonnes
     donnees['nb_voisins'] = np.zeros(len(donnees), dtype=np.int32)
@@ -21,6 +21,7 @@ def run_ball_tree(csv_file, threshold_pir):
 
     # Calcul des voisins avec BallTree
     coords = np.radians(donnees[['LAT', 'LON']].values.astype(np.float32))
+    print(type(coords))
     tree = BallTree(coords, metric='haversine', leaf_size=40)
     radius = 45 / 6371.0
 
@@ -116,82 +117,43 @@ def nb_users_par_cluster(fichier):
     return( moyenne_nb_users)
 
 
-def run_simulation_ball_tree(csv_file, algorithm):
-    if algorithm == "ball_tree":
-        #1 Gbps
-        run_ball_tree("generated.csv", 1000)
-        nb_cluster_mean_1gbps = calcul_nb_clusters("res_clusters_ball_tree.csv")
-        nb_usrs_mean_1gbps = nb_users_par_cluster("res_clusters_ball_tree.csv")
-        #2 Gbps
-        run_ball_tree("generated.csv", 2000)
-        nb_cluster_mean_2gbps = calcul_nb_clusters("res_clusters_ball_tree.csv")
-        nb_usrs_mean_2gbps = nb_users_par_cluster("res_clusters_ball_tree.csv")
-        #4 Gbps
-        run_ball_tree("generated.csv", 4000)
-        nb_cluster_mean_4gbps = calcul_nb_clusters("res_clusters_ball_tree.csv")
-        nb_usrs_mean_4gbps = nb_users_par_cluster("res_clusters_ball_tree.csv")
+def run_simulation_ball_tree(csv_file):
+    #1 Gbps
+    run_ball_tree(csv_file, 1000)
+    nb_cluster_mean_1gbps = calcul_nb_clusters("res_clusters_ball_tree.csv")
+    nb_usrs_mean_1gbps = nb_users_par_cluster("res_clusters_ball_tree.csv")
+    #2 Gbps
+    run_ball_tree(csv_file, 2000)
+    nb_cluster_mean_2gbps = calcul_nb_clusters("res_clusters_ball_tree.csv")
+    nb_usrs_mean_2gbps = nb_users_par_cluster("res_clusters_ball_tree.csv")
+    #4 Gbps
+    run_ball_tree(csv_file, 4000)
+    nb_cluster_mean_4gbps = calcul_nb_clusters("res_clusters_ball_tree.csv")
+    nb_usrs_mean_4gbps = nb_users_par_cluster("res_clusters_ball_tree.csv")
 
-        # Créer un DataFrame avec les informations
-        data = {
-            'Débit': ['1 Gbps', '2 Gbps', '4 Gbps'],
-            'Nb de clusters': [nb_cluster_mean_1gbps, nb_cluster_mean_2gbps, nb_cluster_mean_4gbps],
-            'Nb d\'utilisateurs par cluster': [nb_usrs_mean_1gbps, nb_usrs_mean_2gbps, nb_usrs_mean_4gbps]
-        }
+    # Créer un DataFrame avec les informations
+    data = {
+        'Débit': ['1 Gbps', '2 Gbps', '4 Gbps'],
+        'Nb de clusters': [nb_cluster_mean_1gbps, nb_cluster_mean_2gbps, nb_cluster_mean_4gbps],
+        'Nb d\'utilisateurs par cluster': [nb_usrs_mean_1gbps, nb_usrs_mean_2gbps, nb_usrs_mean_4gbps]
+    }
 
-        df = pd.DataFrame(data)
+    df = pd.DataFrame(data)
 
-        # Enregistrer le DataFrame dans un fichier CSV
-        df.to_csv('stats_ball_tree.csv', index=False)
+    # Enregistrer le DataFrame dans un fichier CSV
+    df.to_csv('stats_ball_tree.csv', index=False)
 
-        print("Pour 1 Gbps :")
-        print("Nombre de clusters :" + str(nb_cluster_mean_1gbps))
-        print("Nombre moyen de terminaux par clusters :" + str(nb_usrs_mean_1gbps))
+    print("Pour 1 Gbps :")
+    print("Nombre de clusters :" + str(nb_cluster_mean_1gbps))
+    print("Nombre moyen de terminaux par clusters :" + str(nb_usrs_mean_1gbps))
 
-        print("Pour 2 Gbps :")
-        print("Nombre de clusters :" + str(nb_cluster_mean_2gbps))
-        print("Nombre moyen de terminaux par clusters :" + str(nb_usrs_mean_2gbps))
+    print("Pour 2 Gbps :")
+    print("Nombre de clusters :" + str(nb_cluster_mean_2gbps))
+    print("Nombre moyen de terminaux par clusters :" + str(nb_usrs_mean_2gbps))
 
-        print("Pour 4 Gbps :")
-        print("Nombre de clusters :" + str(nb_cluster_mean_4gbps))
-        print("Nombre moyen de terminaux par clusters :" + str(nb_usrs_mean_4gbps))
-
-    elif algorithm == "DBSCAN":
-        #1 Gbps
-        run_dbscan("generated.csv", 1000)
-        nb_cluster_mean_1gbps = calcul_nb_clusters("res_clusters_dbscan.csv")
-        nb_usrs_mean_1gbps = nb_users_par_cluster("res_clusters_dbscan.csv")
-        #2 Gbps
-        run_dbscan("generated.csv", 2000)
-        nb_cluster_mean_2gbps = calcul_nb_clusters("res_clusters_dbscan.csv")
-        nb_usrs_mean_2gbps = nb_users_par_cluster("res_clusters_dbscan.csv")
-        #4 Gbps
-        run_dbscan("generated.csv", 4000)
-        nb_cluster_mean_4gbps = calcul_nb_clusters("res_clusters_dbscan.csv")
-        nb_usrs_mean_4gbps = nb_users_par_cluster("res_clusters_dbscan.csv")
-
-        # Créer un DataFrame avec les informations
-        data = {
-            'Débit': ['1 Gbps', '2 Gbps', '4 Gbps'],
-            'Nb de clusters': [nb_cluster_mean_1gbps, nb_cluster_mean_2gbps, nb_cluster_mean_4gbps],
-            'Nb d\'utilisateurs par cluster': [nb_usrs_mean_1gbps, nb_usrs_mean_2gbps, nb_usrs_mean_4gbps]
-        }
-
-        df = pd.DataFrame(data)
-
-        # Enregistrer le DataFrame dans un fichier CSV
-        df.to_csv('stats_ball_tree.csv', index=False)
-
-        print("Pour 1 Gbps :")
-        print("Nombre de clusters :" + str(nb_cluster_mean_1gbps))
-        print("Nombre moyen de terminaux par clusters :" + str(nb_usrs_mean_1gbps))
-
-        print("Pour 2 Gbps :")
-        print("Nombre de clusters :" + str(nb_cluster_mean_2gbps))
-        print("Nombre moyen de terminaux par clusters :" + str(nb_usrs_mean_2gbps))
-
-        print("Pour 4 Gbps :")
-        print("Nombre de clusters :" + str(nb_cluster_mean_4gbps))
-        print("Nombre moyen de terminaux par clusters :" + str(nb_usrs_mean_4gbps))
+    print("Pour 4 Gbps :")
+    print("Nombre de clusters :" + str(nb_cluster_mean_4gbps))
+    print("Nombre moyen de terminaux par clusters :" + str(nb_usrs_mean_4gbps))
 
 #DBSCAN PRENDS TROP DE TEMPS - DONNEES TROP GRANDES
-run_simulation_ball_tree("test_small_csav_500.csv", "ball_tree")
+run_simulation_ball_tree("test_small_csav_500.csv")
