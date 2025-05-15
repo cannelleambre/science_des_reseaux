eb27161dd51d174ace54925a7aa309d78a368a3b
Ambre Liabat <ammbre.liabat@yahoo.com>
2025-05-15 11:50:52 +0200

    impossible de faire amrcher dbscan commeil faut

--- balltree.py
+++ balltree.py
@@ -28,7 +28,7 @@ def run_ball_tree(csv_file, threshold_pir):
     indices = tree.query_radius(coords, r=radius)
     donnees['nb_voisins'] = np.array([len(neighs) - 1 for neighs in indices], dtype=np.int32)
     donnees['VID'] = [list(neighs[neighs != i]) for i, neighs in enumerate(indices)] # voisins id
-
+    print(donnees)
     # Assignation des clusters
     n = 1
     cluster_map = np.zeros(len(donnees), dtype=np.int32)
@@ -53,47 +53,3 @@ def run_ball_tree(csv_file, threshold_pir):
     donnees.to_csv("res/res_clusters_ball_tree.csv", sep=',', index=False)
     #generation des couleurs
     color_csv("res/res_clusters_ball_tree.csv")
-
-
-
-
-def run_simulation_ball_tree(csv_file):
-    #1 Gbps
-    run_ball_tree(csv_file, 1000)
-    nb_cluster_mean_1gbps = calcul_nb_clusters("res/res_clusters_ball_tree.csv")
-    nb_usrs_mean_1gbps = nb_users_par_cluster("res/res_clusters_ball_tree.csv")
-    #2 Gbps
-    run_ball_tree(csv_file, 2000)
-    nb_cluster_mean_2gbps = calcul_nb_clusters("res/res_clusters_ball_tree.csv")
-    nb_usrs_mean_2gbps = nb_users_par_cluster("res/res_clusters_ball_tree.csv")
-    #4 Gbps
-    run_ball_tree(csv_file, 4000)
-    nb_cluster_mean_4gbps = calcul_nb_clusters("res/res_clusters_ball_tree.csv")
-    nb_usrs_mean_4gbps = nb_users_par_cluster("res/res_clusters_ball_tree.csv")
-
-    # Créer un DataFrame avec les informations
-    data = {
-        'Débit': ['1 Gbps', '2 Gbps', '4 Gbps'],
-        'Nb de clusters': [nb_cluster_mean_1gbps, nb_cluster_mean_2gbps, nb_cluster_mean_4gbps],
-        'Nb d\'utilisateurs par cluster': [nb_usrs_mean_1gbps, nb_usrs_mean_2gbps, nb_usrs_mean_4gbps]
-    }
-
-    df = pd.DataFrame(data)
-
-    # Enregistrer le DataFrame dans un fichier CSV
-    df.to_csv('stats_ball_tree.csv', index=False)
-
-    #print("Pour 1 Gbps :")
-    #print("Nombre de clusters :" + str(nb_cluster_mean_1gbps))
-    #print("Nombre moyen de terminaux par clusters :" + str(nb_usrs_mean_1gbps))
-
-    #print("Pour 2 Gbps :")
-    #print("Nombre de clusters :" + str(nb_cluster_mean_2gbps))
-    #print("Nombre moyen de terminaux par clusters :" + str(nb_usrs_mean_2gbps))
-
-    #print("Pour 4 Gbps :")
-    #print("Nombre de clusters :" + str(nb_cluster_mean_4gbps))
-    #print("Nombre moyen de terminaux par clusters :" + str(nb_usrs_mean_4gbps))
-
-
-#run_simulation_ball_tree("test_small_csav_500.csv")
--- dbscan.py
+++ dbscan.py
@@ -12,16 +12,29 @@ def run_dbscan(csv_file, max_pir_threshold):
         'LON': np.float32
     }
     donnees = pd.read_csv(csv_file, delimiter=",", dtype=dtype_dict)
+    donnees_temp = donnees
+    # Initialisation des colonnes
+    donnees['nb_voisins'] = np.zeros(len(donnees), dtype=np.int32)
+    donnees['cluster'] = np.zeros(len(donnees), dtype=np.int32)
+    donnees['VID'] = [[] for _ in range(len(donnees))]
+
 
     # Calcul des clusters avec DBSCAN
     coords = np.radians(donnees[['LAT', 'LON']].values.astype(np.float32))
    
      # Calcul des clusters avec DBSCAN
     coords = np.radians(donnees[['LAT', 'LON']].values.astype(np.float32))
-    dbscan = DBSCAN(eps=45/6371.0, min_samples=10)  # eps est la distance maximale entre les points dans un cluster
+    dbscan = DBSCAN(eps=45, min_samples=1)  # eps est la distance maximale entre les points dans un cluster
     labels = dbscan.fit_predict(coords)
 
-   # Assignation des clusters
+
+    donnees['cluster'] = dbscan.labels_
+    donnees['nb_voisins'] = donnees.groupby('cluster')['cluster'].transform('count') - 1
+    donnees = donnees.reset_index()
+    donnees['VID'] = donnees.apply(lambda row: donnees[(donnees['cluster'] == row['cluster']) & (donnees['index'] != row['index'])]['index'].tolist(), axis=1)
+    if donnees_temp.equals(donnees):
+        print("J'EN AI MAAAAAAARES")
+    # Assignation des clusters
     n = 1
     cluster_map = np.zeros(len(donnees), dtype=np.int32)
     sorted_indices = donnees['PIR'].sort_values(ascending=False).index
@@ -29,17 +42,15 @@ def run_dbscan(csv_file, max_pir_threshold):
     for index in sorted_indices:
         if cluster_map[index] == 0:
             cluster_map[index] = n
-            voisins = []
-            for i, label in enumerate(labels):
-                if label == labels[index] and i != index:
-                    voisins.append(i)
+            voisins = donnees.at[index, 'VID']
             pir_somme = donnees.at[index, 'PIR']
             for voisin in voisins:
                 pir_somme += donnees.at[voisin, 'PIR']
                 if pir_somme > max_pir_threshold:
                     break
             if pir_somme <= max_pir_threshold:
-                cluster_map[voisins] = n            
+                for voisin in voisins:
+                    cluster_map[voisin] = n
             n += 1
 
     donnees['cluster'] = cluster_map
